c698e1b0fcf05fd6370623bbba7efb5e51ad9127
João Victor <joaodjtr@gmail.com>
2019-06-14 23:54:21 -0300

    home and overlay effect

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+function showSection(s, n) {
+  var section = document.getElementById(s);
+  var overlay = document.getElementsByClassName("overlay-effect")[n];
+  overlay.style.left = 0;
+  overlay.style.transition = ".25s linear";
+  section.style.left = 0;
+  section.style.transition = ".35s linear";
+}
+
+function hideSection(s, n) {
+  var section = document.getElementById(s);
+  var overlay = document.getElementsByClassName("overlay-effect")[n];
+  section.style.left = "100%";
+  section.style.transition = ".25s linear";
+  overlay.style.left = "100%";
+  overlay.style.transition = ".35s linear";
+}
