04121a85b822c0f05de93ca91c227bc909e1519a
João Victor <joaodjtr@gmail.com>
2019-08-08 08:18:11 -0300

    v.2

--- scripts/script.js
+++ scripts/script.js
@@ -1,71 +1,65 @@
+var inputs = document.getElementsByClassName("input-form");
+var labels = document.getElementsByClassName("label-form");
+var btn = document.getElementById("button-form");
 function init() {
-  var didScroll = false;
-  var lastScrollPosition;
-  var actualScrollPosition;
-  var landingPosition = document.getElementById("landing").getBoundingClientRect().bottom + 300;
-  var whoWeAreScroll = document.getElementById("who-we-are").getBoundingClientRect().top - 100;
-  var teamScroll = document.getElementById("team").getBoundingClientRect().top - 100;
-  var worksScroll = document.getElementById("works").getBoundingClientRect().top - 100;
-  var contactScroll = document.getElementById("contact").getBoundingClientRect().top - 400; /* FUTURAMENTE EDITÁVEL*/;
-  var links = document.getElementsByClassName("link-menu");
-  var trueActualScrollPosition;
-  var header = document.getElementById("fixed-header");
-
-  window.addEventListener("scroll", function() {
-    trueActualScrollPosition = window.scrollY;
-    checksPositionSection(trueActualScrollPosition);
-    var x = trueActualScrollPosition - landingPosition;
-    this.console.log(x);
-    if(x > -300){
-      if (!didScroll) {
-        didScroll = true;
-        actualScrollPosition = window.scrollY;
-      } else {
-        didScroll = false;
-        lastScrollPosition = window.scrollY;
-        if (lastScrollPosition < actualScrollPosition) {
-          header.style.top = 0;
-          header.style.height = "100px";
-        } else {
-          header.style.top = 0;
-          header.style.height = "75px";
-        }
-      }
-    }else if(x < -500){
-      header.style.top = "-130px";
-    }
-    
+  window.addEventListener("scroll", () => {
+    verifiesHeader();
+    changesHeaderLinks()
   });
 
-  for(var i = 0; i < links.length; i++){
-    links[i].addEventListener("click", function() {
-      resetStyleLinks();
-      links[i-1].style.color = "#1ab7ea";
-      
-    });
-  }
+  labelsForm()
+}
 
-  function checksPositionSection(scroll){
-    if(scroll > contactScroll){
-      resetStyleLinks();
-      links[3].style.color = "#1ab7ea";
-    }else if(scroll > worksScroll){
-      resetStyleLinks();
-      links[2].style.color = "#1ab7ea";
-    }else if(scroll > teamScroll){
-      resetStyleLinks();
-      links[1].style.color = "#1ab7ea";
-    }else if(scroll > whoWeAreScroll){
-      resetStyleLinks();
-      links[0].style.color = "#1ab7ea";
+function changesHeaderLinks () {
+  sections = document.getElementsByClassName('section')
+  links = document.getElementsByClassName('link-header')
+  for(i = 0; i < sections.length; i++){
+    sections[i].positionY = sections[i].offsetTop;
+    sections[3].positionY -= 500
+    if(window.scrollY > sections[i].positionY){
+      for(j = 0; j < links.length; j++){
+        links[j].style.fontWeight = "normal";
+        if(j == i) links[j].style.fontWeight = "bold";       
+      }
     }
   }
-
-  function resetStyleLinks() {
-    for(var i = 0; i < links.length; i++){
-      links[i].style.color = "#103f4e";
-    }
+  
+  for(i = 0; i < sections.length; i++){
+    // if(window.scrollY > sections[0].positionY) console.log("olá")
+    // if(window.scrollY > sections[1].positionY) console.log("td bem")
+    // if(window.scrollY > sections[2].positionY) console.log("tchau")
+    // if(window.scrollY > sections[3].positionY) console.log(sections[0].positionY)
   }
 }
 
+function verifiesHeader() {
+  elementDefault = document.querySelector("#landing-header");
+  elementDefault.positionY = document.querySelector("#landing").offsetTop;
+  header = document.querySelector("#header");
+  if (window.scrollY > elementDefault.positionY) header.style.top = 0;
+  if (window.scrollY < elementDefault.positionY) header.style.top = "-100%";
+}
 
+function labelsForm(){
+  for (i = 0; i < inputs.length; i++) {
+    inputs[i].orderNumber = i;
+    inputs[i].addEventListener("input", e => {
+      e = e.target;
+      if (e.value != "") {
+        labels[e.orderNumber].style.color = "rgba(0,0,0,0.5)";
+      } else {
+        labels[e.orderNumber].style.color = "black";
+      }
+      checksForm();
+    });
+  }
+}
+
+function checksForm() {
+  var c = true;
+  for (i = 0; i < inputs.length; i++) {
+    if (inputs[i].value == "") c = false;
+  }
+  if (c) btn.disabled = false;
+  if (!c) btn.disabled = true;
+}
